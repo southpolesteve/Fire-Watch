ef328d8fd4d0919ce97519dfa6008ddd86cf801f
Steve Faulkner <southpolesteve@gmail.com>
2011-05-02 16:33:25 -0500

    Blank rails app for firewatch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firewatch::Application.config.secret_token = '986aba07ce7283e6db1ac90783721e6591d1a2910fa55db779465f0ed8e99e41802342dd20f475297009dded7ed07ac18f782659cf5ea115274cab437ccb2ef1'
